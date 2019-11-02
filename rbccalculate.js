89bfd06f70ee7c98f7d0ea8b2d4a240caf9ef09d
Tommy Nguyen <TommyNguyen@Tommys-MacBook-Pro.local>
2019-11-02 14:28:40 -0500

    ordering

---
+++ rbccalculate.js
@@ -0,0 +1,26 @@
+function rbccalculate()
+{
+  var weight = document.getElementById("rbcWeight").value;
+  //var weight = 10;
+  var minRec = 0;
+  var maxRec = 0;
+  var minRange = 10;
+  var maxRange = 15;
+  var recommendation = "he";
+
+  if (weight <20) {
+  	minRec = weight * minRange;
+  	maxRec = weight * maxRange;
+  	recommendation = minRec + "ml - " + maxRec +"ml";
+	}
+
+  if (weight >= 20){
+  	recommendation = "1 unit";
+  }
+
+  alert(recommendation);
+
+}
+
+
+rbccalculate();
